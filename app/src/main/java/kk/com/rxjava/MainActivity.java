a8ca4553891a31b00f16629256f70e46ae5c8f9e
liuhuijie <857166619@qq.com>
2017-03-23 15:19:58 +0800

    关联rx

--- app/src/main/java/kk/com/rxjava/MainActivity.java
+++ app/src/main/java/kk/com/rxjava/MainActivity.java
@@ -1,13 +1,64 @@
 package kk.com.rxjava;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sub();
     }
+
+    private void sub() {
+
+        //创建一个上游 Observable：
+        Observable<Integer> observable = Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
+                emitter.onNext(1);
+                emitter.onNext(2);
+                emitter.onNext(3);
+                emitter.onComplete();
+            }
+        });
+        //创建一个下游 Observer
+        Observer<Integer> observer = new Observer<Integer>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.e(TAG, "subscribe");
+            }
+
+            @Override
+            public void onNext(Integer value) {
+                Log.e(TAG, "" + value);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.e(TAG, "error");
+            }
+
+            @Override
+            public void onComplete() {
+                Log.e(TAG, "complete");
+            }
+        };
+        //建立连接
+        observable.subscribe(observer);
+
+    }
+
+
 }
